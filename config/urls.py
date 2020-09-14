ded712bc1851b55b1199201ffcd5649bde02cfb0
Florian Alu <alu@prolibre.com>
2020-09-14 13:59:42 +0200

    Fix Flake8 Errors

--- config/urls.py
+++ config/urls.py
@@ -3,20 +3,21 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views import defaults as default_views
-from django.views.generic import TemplateView
-from rest_framework.authtoken.views import obtain_auth_token
+from nobinobi_child import urls as nobinobi_child_urls
 from nobinobi_core import urls as nobinobi_core_urls
 from nobinobi_staff import urls as nobinobi_staff_urls
-from nobinobi_child import urls as nobinobi_child_urls
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-                  path('', include(nobinobi_core_urls)),
-                  path('', include(nobinobi_staff_urls)),
-                  path('', include(nobinobi_child_urls)),
-                  # Django Admin, use {% url 'admin:index' %}
-                  path(settings.ADMIN_URL, admin.site.urls),
-                  # Your stuff: custom urls includes go here
-              ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', include(nobinobi_core_urls)),
+    path('', include(nobinobi_staff_urls)),
+    path('', include(nobinobi_child_urls)),
+    # Django Admin, use {% url 'admin:index' %}
+    path(settings.ADMIN_URL, admin.site.urls),
+    # Your stuff: custom urls includes go here
+]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # API URLS
 urlpatterns += [
